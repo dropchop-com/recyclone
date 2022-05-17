0eeb1ab4671ae04a1b3bf90c1722f7fe88461526
Nikola Ivačič <nikola.ivacic@dropchop.com>
2022-05-17 10:39:28 +0200

    Refactored first level (embedded) translations not to mix with HasTranslation* marker interfaces.

--- model/api/src/main/java/com/dropchop/recyclone/model/api/localization/TitleTranslation.java
+++ model/api/src/main/java/com/dropchop/recyclone/model/api/localization/TitleTranslation.java
@@ -1,9 +1,11 @@
 package com.dropchop.recyclone.model.api.localization;
 
+import com.dropchop.recyclone.model.api.marker.HasTitle;
+
 /**
  * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 10. 01. 22.
  */
-public interface TitleTranslation extends Translation {
+public interface TitleTranslation extends Translation, HasTitle {
   String getTitle();
   void setTitle(String title);
 }
