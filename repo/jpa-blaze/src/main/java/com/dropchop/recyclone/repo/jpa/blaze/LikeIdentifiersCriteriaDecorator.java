e303687b4d597f3cce93d03fd3d32502daf17ac2
Nikola Ivačič <nikola.ivacic@dropchop.com>
2022-07-18 12:33:56 +0200

    Removed code duplication for string like queries.

--- repo/jpa-blaze/src/main/java/com/dropchop/recyclone/repo/jpa/blaze/LikeIdentifiersCriteriaDecorator.java
+++ repo/jpa-blaze/src/main/java/com/dropchop/recyclone/repo/jpa/blaze/LikeIdentifiersCriteriaDecorator.java
@@ -1,7 +1,5 @@
 package com.dropchop.recyclone.repo.jpa.blaze;
 
-import com.blazebit.persistence.CriteriaBuilder;
-import com.blazebit.persistence.WhereOrBuilder;
 import com.dropchop.recyclone.model.api.invoke.CodeParams;
 import com.dropchop.recyclone.model.api.invoke.IdentifierParams;
 import com.dropchop.recyclone.model.api.invoke.Params;
@@ -11,7 +9,7 @@ import java.util.List;
 /**
  * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 3. 03. 22.
  */
-public class LikeIdentifierCriteriaDecorator extends BlazeCriteriaDecorator {
+public class LikeIdentifiersCriteriaDecorator extends LikeListCriteriaDecorator {
 
   @Override
   public void decorate() {
@@ -30,27 +28,6 @@ public class LikeIdentifierCriteriaDecorator extends BlazeCriteriaDecorator {
       return;
     }
 
-    if (ids.isEmpty()) {
-      return;
-    }
-
-    boolean enableLike = false;
-    for (String id : ids) {
-      if (id.endsWith("*")) {
-        enableLike = true;
-        break;
-      }
-    }
-
-    CriteriaBuilder<?> cb = getContext().getCriteriaBuilder();
-    if (!enableLike) {
-      cb.where(idColName).in(ids);
-    } else {
-      WhereOrBuilder<? extends CriteriaBuilder<?>> wob = cb.whereOr();
-      for (String id : ids) {
-        wob.where(idColName).like().value(id.replace("*", "%").replace("?", "_")).noEscape();
-      }
-      wob.endOr();
-    }
+    decorateWithListOfStringsAsLike(ids, idColName);
   }
 }
---
+++ repo/jpa-blaze/src/main/java/com/dropchop/recyclone/repo/jpa/blaze/LikeTypeCriteriaDecorator.java
@@ -0,0 +1,29 @@
+package com.dropchop.recyclone.repo.jpa.blaze;
+
+import com.dropchop.recyclone.model.api.invoke.Params;
+import com.dropchop.recyclone.model.dto.invoke.TypeParams;
+
+import java.util.List;
+
+/**
+ * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 18. 07. 22.
+ */
+public class LikeTypeCriteriaDecorator extends LikeListCriteriaDecorator {
+
+  @Override
+  public void decorate() {
+    Params params = getContext().getParams();
+    List<String> types;
+
+    String alias = getContext().getRootAlias();
+    String typeColName = alias + ".type";
+
+    if (params instanceof TypeParams) {
+      types = ((TypeParams) params).getTypes();
+    } else {
+      return;
+    }
+
+    decorateWithListOfStringsAsLike(types, typeColName);
+  }
+}
