d550b5760e6e9b3404c5ac0ba03df6245499e697
Nikola Ivačič <nikola.ivacic@dropchop.com>
2022-08-27 07:11:48 +0200

    Added new ByIdService interface not restricted to entity.

---
+++ service/api/src/main/java/com/dropchop/recyclone/service/api/ByIdService.java
@@ -0,0 +1,20 @@
+package com.dropchop.recyclone.service.api;
+
+import com.dropchop.recyclone.model.api.base.Model;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 10. 03. 22.
+ */
+public interface ByIdService<M extends Model, ID> extends Service {
+
+  Class<M> getRootClass();
+
+  M findById(ID id);
+
+  List<M> findById(Collection<ID> ids);
+
+  List<M> findAll();
+}
--- service/api/src/main/java/com/dropchop/recyclone/service/api/EntityByIdService.java
+++ service/api/src/main/java/com/dropchop/recyclone/service/api/EntityByIdService.java
@@ -3,21 +3,10 @@ package com.dropchop.recyclone.service.api;
 import com.dropchop.recyclone.model.api.base.Dto;
 import com.dropchop.recyclone.model.api.base.Entity;
 
-import java.util.Collection;
-import java.util.List;
-
 /**
  * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 10. 03. 22.
  */
-public interface EntityByIdService<D extends Dto, E extends Entity, ID> extends Service {
-
-  Class<E> getRootClass();
+public interface EntityByIdService<D extends Dto, E extends Entity, ID> extends ByIdService<E, ID> {
 
   E findById(D dto);
-
-  E findById(ID id);
-
-  List<E> findById(Collection<ID> ids);
-
-  List<E> findAll();
 }
