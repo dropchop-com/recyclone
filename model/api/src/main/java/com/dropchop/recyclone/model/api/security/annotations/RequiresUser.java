e54357cbd20703983821dd42f6fe577f606badcd
Nikola Ivačič <nikola.ivacic@dropchop.com>
2023-05-26 07:29:30 +0200

    Working on shiro integration.

---
+++ model/api/src/main/java/com/dropchop/recyclone/model/api/security/annotations/RequiresUser.java
@@ -0,0 +1,14 @@
+package com.dropchop.recyclone.model.api.security.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Modeled after Shiro annotations: org.apache.shiro.authz.annotation.RequiresUser
+ *
+ * @author Nikola Ivačič <nikola.ivacic@dropchop.org> on 23. 05. 23.
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RequiresUser {
+}
