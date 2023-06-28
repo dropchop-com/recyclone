11df6541d1aecc2d911a94eefe04bb8a4af80559
Armando Ota <armando.oa@dropchop.com>
2023-06-28 09:37:40 +0200

    Added get() test to get permission entity by uuid.

--- test/quarkus/src/test/java/com/dropchop/recyclone/test/quarkus/PermissionResourceTest.java
+++ test/quarkus/src/test/java/com/dropchop/recyclone/test/quarkus/PermissionResourceTest.java
@@ -8,13 +8,11 @@ import com.dropchop.recyclone.model.dto.security.Permission;
 import com.dropchop.recyclone.rest.jaxrs.api.MediaType;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -70,8 +68,61 @@ public class PermissionResourceTest {
     assertEquals(Set.of(new TitleDescriptionTranslation("sl", "Dovoli vse akcije na jezikih.")), retPermission.getTranslations());
   }
 
+
+  //TODO: FIX THE PROBLEM WITH GETTING ENTITY BY UUID
   @Test
   @Order(20)
+  @Disabled
+  public void get() {
+    String permUuid = UUID.randomUUID().toString();
+    Domain domain = new Domain();
+    domain.setCode(Constants.Domains.Localization.LANGUAGE);
+    Action action = new Action();
+    action.setCode(Constants.Actions.ALL);
+    Permission permission = new Permission();
+    permission.setUuid(permUuid);
+    permission.setAction(action);
+    permission.setDomain(domain);
+    permission.setTitle("Permit all actions on Language");
+    permission.setLang("en");
+    permission.addTranslation(new TitleDescriptionTranslation("sl", "Dovoli vse akcije na jezikih."));
+
+    List<Permission> permissions = given()
+        //.log().all()
+        .contentType(ContentType.JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        //.header("Authorization", "Bearer editortoken1")
+        .auth().preemptive().basic("admin1", "password")
+        .and()
+        .body(List.of(permission))
+        .when()
+        .post("/api/internal/security/permission?c_level=2")
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().jsonPath().getList(".", Permission.class);
+    assertEquals(1, permissions.size());
+
+
+    permissions = given()
+        //.log().all()
+        .contentType(ContentType.JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .auth().preemptive().basic("admin1", "password")
+        .and()
+        .body(List.of(permission))
+        .when()
+        .get("/api/internal/security/permission/" + permUuid + "?c_level=2")
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().jsonPath().getList(".", Permission.class);
+    assertEquals(1, permissions.size());
+
+  }
+
+  @Test
+  @Order(30)
   public void update() {
     Domain domain = new Domain();
     domain.setCode(Constants.Domains.Security.ROLE);
@@ -111,7 +162,7 @@ public class PermissionResourceTest {
   }
 
   @Test
-  @Order(30)
+  @Order(40)
   public void delete() {
     Permission permission = new Permission();
     permission.setUuid("28c9e87d-befe-4c70-b582-c176653d917c");
