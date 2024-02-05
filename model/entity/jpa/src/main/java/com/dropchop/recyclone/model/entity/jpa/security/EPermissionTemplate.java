ba443b0b562093fe1f6d75731da8b25c7e9c58f0
Armando Ota <armando.ota@dropchop.com>
2024-02-05 19:28:23 +0100

    Removed inheritance to EPermissionInstance and added missing properties due to the fact that hibernate merged both entities into same table.

--- model/entity/jpa/src/main/java/com/dropchop/recyclone/model/entity/jpa/security/EPermissionTemplate.java
+++ model/entity/jpa/src/main/java/com/dropchop/recyclone/model/entity/jpa/security/EPermissionTemplate.java
@@ -1,10 +1,9 @@
 package com.dropchop.recyclone.model.entity.jpa.security;
 
 import com.dropchop.recyclone.model.api.security.PermissionTemplate;
+import com.dropchop.recyclone.model.entity.jpa.base.EUuid;
 import com.dropchop.recyclone.model.entity.jpa.localization.ETitleDescriptionTranslation;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,8 +15,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name = "security_permission_template")
-public class EPermissionTemplate extends EPermissionInstance implements PermissionTemplate<EPermission,
+public class EPermissionTemplate extends EUuid implements PermissionTemplate<EPermission,
   ETitleDescriptionTranslation, EAction, EDomain> {
+
+  @ManyToOne
+  @JoinColumn(name = "fk_permission_uuid", referencedColumnName = "uuid", foreignKey = @ForeignKey(name = "permission_fk"))
+  private EPermission permission;
+
+
+  @Column(name = "subject")
+  private String subject;
+
+
+  @Column(name = "subject_uuid")
+  private UUID subjectId;
+
+
+  @Column(name = "allowed")
+  private Boolean allowed;
 
   @Column(name = "sub_subject")
   private String subSubject;
