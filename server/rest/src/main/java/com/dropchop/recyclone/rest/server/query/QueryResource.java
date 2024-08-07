e4c621e033f588b66d0b30e27a24f7fb557b1a2a
Samo Pritrznik <pritrznik.samo@gmail.com>
2024-08-07 08:43:09 +0200

    https://github.com/dropchop-com/recyclone/issues/5 Minor changes to QueryResource, without warnings. #5

--- server/rest/src/main/java/com/dropchop/recyclone/rest/server/query/QueryResource.java
+++ server/rest/src/main/java/com/dropchop/recyclone/rest/server/query/QueryResource.java
@@ -3,18 +3,29 @@ package com.dropchop.recyclone.rest.server.query;
 import com.dropchop.recyclone.model.dto.invoke.CodeParams;
 import com.dropchop.recyclone.model.dto.query.Query;
 import com.dropchop.recyclone.model.dto.rest.Result;
-import com.dropchop.recyclone.rest.server.ClassicRestResource;
+import com.dropchop.recyclone.rest.server.ClassicReadByCodeResource;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
 @RequestScoped
 @SuppressWarnings("CdiInjectionPointsInspection")
-public class QueryResource implements com.dropchop.recyclone.rest.api.query.QueryResource, ClassicRestResource<Query> {
+public class QueryResource extends ClassicReadByCodeResource<Query, CodeParams> implements com.dropchop.recyclone.rest.api.query.QueryResource {
 
   @Inject
-  //QueryService service;
+  CodeParams codeParams;
+
+  /*@Inject
+  QueryService queryService;*/
+
+  @Override
+  public Result<Query> getByCode(String code) {
+    codeParams.setCodes(List.of(code));
+    return null;
+  }
 
   @Override
   public Result<Query> getAll() {
@@ -43,6 +54,16 @@ public class QueryResource implements com.dropchop.recyclone.rest.api.query.Quer
 
   @Override
   public Result<?> executeQuery(Query params) {
+    return null;
+  }
+
+  @Override
+  public Result<Query> get() {
+    return null;
+  }
+
+  @Override
+  public Result<Query> search(CodeParams parameters) {
     return null;
   }
 }
