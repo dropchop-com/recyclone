9f9b7a42097c47c87186232a7c868797dfd871da
Nikola Ivačič <nikola.ivacic@dropchop.com>
2024-10-03 14:36:29 +0200

    Refactored to better json.
    https://github.com/dropchop-com/recyclone/issues/9

--- common/rest/runtime/src/test/java/com/dropchop/recyclone/rest/jackson/client/AggregationDeserializerTest.java
+++ common/rest/runtime/src/test/java/com/dropchop/recyclone/rest/jackson/client/AggregationDeserializerTest.java
@@ -1,6 +1,7 @@
 package com.dropchop.recyclone.rest.jackson.client;
 
 import com.dropchop.recyclone.model.api.query.Aggregation;
+import com.dropchop.recyclone.model.api.query.aggregation.AggregationContainer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -13,7 +14,7 @@ public class AggregationDeserializerTest {
 
   @Test
   public void testAggregationCompositionTest() throws Exception {
-    List<Aggregation> a = List.of(
+    List<AggregationContainer> a = Aggregation.aggs(
       max(
         "watch_max",
         "watch",
@@ -55,47 +56,65 @@ public class AggregationDeserializerTest {
     String jsonOutput1 = mapper.writeValueAsString(a);
     System.out.printf(mapper.writeValueAsString(jsonOutput1));
     String expected = """
-      [ {
-        "aggs" : [ {
-          "$sum" : {
-            "aggs" : [ ],
-            "name" : "nested_worker_sum",
-            "field" : "worker"
-          }
-        }, {
-          "$min" : {
-            "aggs" : [ ],
-            "name" : "nested_worker_min",
-            "field" : "worker"
-          }
-        }, {
-          "$avg" : {
-            "aggs" : [ ],
-            "name" : "nested_worker_avg",
-            "field" : "worker"
-          }
-        }, {
-          "$count" : {
-            "aggs" : [ ],
-            "name" : "nested_nested_worker_count",
-            "field" : "worker"
-          }
-        } ],
-        "name" : "watch_max",
-        "field" : "watch"
-      }, {
-        "aggs" : [ ],
-        "name" : "nested_nested_worker_cardinality",
-        "field" : "worker"
-      }, {
-        "aggs" : [ ],
-        "name" : "nested_nested_worker_dateHistogram",
-        "field" : "worker"
-      }, {
-        "aggs" : [ ],
-        "name" : "nested_worker_terms",
-        "field" : "worker"
-      } ]""";
+        [
+         {
+           "$max": {
+             "aggs": [
+               {
+                 "$sum": {
+                   "aggs": [],
+                   "name": "nested_worker_sum",
+                   "field": "worker"
+                 }
+               },
+               {
+                 "$min": {
+                   "aggs": [],
+                   "name": "nested_worker_min",
+                   "field": "worker"
+                 }
+               },
+               {
+                 "$avg": {
+                   "aggs": [],
+                   "name": "nested_worker_avg",
+                   "field": "worker"
+                 }
+               },
+               {
+                 "$count": {
+                   "aggs": [],
+                   "name": "nested_nested_worker_count",
+                   "field": "worker"
+                 }
+               }
+             ],
+             "name": "watch_max",
+             "field": "watch"
+           }
+         },
+         {
+           "$cardinality": {
+             "aggs": [],
+             "name": "nested_nested_worker_cardinality",
+             "field": "worker"
+           }
+         },
+         {
+           "$dateHistogram": {
+             "aggs": [],
+             "name": "nested_nested_worker_dateHistogram",
+             "field": "worker"
+           }
+         },
+         {
+           "$terms": {
+             "aggs": [],
+             "name": "nested_worker_terms",
+             "field": "worker"
+           }
+         }
+       ]""";
     JSONAssert.assertEquals(expected, jsonOutput1, true);
   }
 
