40a76aec690023148f7edecc284f5c19837b1596
Nikola Ivačič <nikola.ivacic@dropchop.com>
2024-10-03 11:42:16 +0200

    Working on tests.
    https://github.com/dropchop-com/recyclone/issues/9

--- common/rest/runtime/src/test/java/com/dropchop/recyclone/rest/jackson/client/AggregationDeserializerTest.java
+++ common/rest/runtime/src/test/java/com/dropchop/recyclone/rest/jackson/client/AggregationDeserializerTest.java
@@ -4,12 +4,50 @@ import com.dropchop.recyclone.model.api.query.Aggregation;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.util.List;
 
 import static com.dropchop.recyclone.model.api.query.Aggregation.*;
 
 public class AggregationDeserializerTest {
+
+  @Test
+  public void testAggregationCompositionTest() throws Exception {
+    List<Aggregation> a = List.of(
+        max(
+            aggregationField("watch_max", "watch"),
+            sum(
+                aggregationField("nested_worker_sum", "worker")
+            ),
+            min(
+                aggregationField("nested_worker_min", "worker")
+            ),
+            avg(
+                aggregationField("nested_worker_avg", "worker"),
+                count(
+                    aggregationField("nested_nested_worker_count", "worker")
+                ),
+                cardinality(
+                    aggregationField("nested_nested_worker_cardinality", "worker")
+                ),
+                dateHistogram(
+                    aggregationHistogramField("nested_nested_worker_dateHistogram", "worker", "month")
+                )
+            )
+        ),
+        terms(
+            aggregationField("worker_terms", "worker")
+        )
+    );
+
+    ObjectMapperFactory mapperFactory = new ObjectMapperFactory();
+    ObjectMapper mapper = mapperFactory.createObjectMapper();
+
+    String jsonOutput1 = mapper.writeValueAsString(a);
+    String expected = "";
+    //JSONAssert.assertEquals(expected, jsonOutput1, false);
+  }
 
   @Test
   public void testAggregationDeserialization() throws Exception {
