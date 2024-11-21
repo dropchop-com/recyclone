72b46d6bf4f65bef3a3562b71714bc7a330662b7
Armando Ota <armando.ota@dropchop.com>
2024-11-21 15:35:29 +0100

    Implementing first level security permissions loading tests

--- server/quarkus/integration-tests/src/test/java/com/dropchop/recyclone/quarkus/it/rest/security/SecurityLoadingTest.java
+++ server/quarkus/integration-tests/src/test/java/com/dropchop/recyclone/quarkus/it/rest/security/SecurityLoadingTest.java
@@ -22,7 +22,7 @@ import static com.dropchop.recyclone.model.api.rest.Constants.Paths.Security.*;
 import static com.dropchop.recyclone.quarkus.it.rest.security.SecurityLoadingTest.Data.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -115,6 +115,14 @@ public class SecurityLoadingTest {
   }
 
 
+  private List<Permission> getPermissions() {
+    Permission permissionView = SecurityHelper.permissionOf(PERMISSION3, DOMAIN_PROJECTS, Constants.Actions.VIEW);
+    Permission permissionCreate = SecurityHelper.permissionOf(PERMISSION1, DOMAIN_PROJECTS, Constants.Actions.CREATE);
+    Permission permissionUpdate = SecurityHelper.permissionOf(PERMISSION2, DOMAIN_PROJECTS, Constants.Actions.UPDATE);
+    Permission permissionDelete = SecurityHelper.permissionOf(PERMISSION4, DOMAIN_PROJECTS, Constants.Actions.DELETE);
+    return List.of(permissionView, permissionCreate, permissionUpdate, permissionDelete);
+  }
+
   private List<Permission> prepPermissions() {
 
     Permission permissionView = SecurityHelper.permissionOf(PERMISSION1, DOMAIN_PROJECTS, Constants.Actions.VIEW);
@@ -156,10 +164,14 @@ public class SecurityLoadingTest {
     this.prepDomain();
     List<Permission> permissions = this.prepPermissions();
 
+    //template node for organizations
     RoleNode roleNodeOrgTemplate = SecurityHelper.roleNodeOf(ORG_TEMPLATE_ROLE_NODE_ID, ORG_ENTITY, null, null,null);
-    RoleNode roleNodeOrgUnitTemplate = SecurityHelper.roleNodeOf(ORG_UNIT_TEMPLATE_ROLE_NODE_ID, ORG_UNIT_ENTITY, null, null,null);
-
+    //organization template permissions
     List<RoleNodePermission> roleNodeOrgPermissions = this.prepRoleNodePermissions(roleNodeOrgTemplate, permissions, ORG_ENTITY, null, true);
+
+    //template node for organization units
+    RoleNode roleNodeOrgUnitTemplate = SecurityHelper.roleNodeOf(ORG_UNIT_TEMPLATE_ROLE_NODE_ID, ORG_UNIT_ENTITY, null, null,null);
+    //organization unit template permissions
     List<RoleNodePermission> roleNodeOrgUnitPermissions = this.prepRoleNodePermissions(roleNodeOrgUnitTemplate, permissions, ORG_UNIT_ENTITY, null, true);
 
     //store templates for org role node
@@ -255,6 +267,10 @@ public class SecurityLoadingTest {
       .extract()
       .body().jsonPath().getList("data", RoleNodePermission.class);
     assertEquals(4, loadedOrgTemplatePermissions.size());
+    for (RoleNodePermission p : loadedOrgTemplatePermissions) {
+      assertInstanceOf(RoleNodePermissionTemplate.class, p);
+      assertNotNull(((RoleNodePermissionTemplate)p).getTarget());
+    }
 
     RoleNodeParams orgUnitParams = new RoleNodeParams();
     orgUnitParams.setTarget(ORG_UNIT_ENTITY);
@@ -273,10 +289,88 @@ public class SecurityLoadingTest {
       .extract()
       .body().jsonPath().getList("data", RoleNodePermission.class);
     assertEquals(4, loadedOrgUnitTemplatePermissions.size());
-
-
+    for (RoleNodePermission p : loadedOrgUnitTemplatePermissions) {
+      assertInstanceOf(RoleNodePermissionTemplate.class, p);
+      assertNotNull(((RoleNodePermissionTemplate)p).getTarget());
+    }
   }
 
+
+  /**
+   * Will check if organizations templates for organization unit are correctly loaded
+   */
+
+  @Test
+  @Order(20)
+  public void testLoadFirstLevelPermissionTemplates() {
+    //organization instance role node
+    RoleNode organizationRoleNode =
+        SecurityHelper.roleNodeOf(ORG_ROLE_NODE_ID, ORG_ENTITY, null, ORG_ENTITY, ORG_ENTITY_ID);
+    //organization unit instance role node
+    RoleNode organizationUnitRoleNode =
+        SecurityHelper.roleNodeOf(ORG_UNIT_ROLE_NODE_ID, ORG_UNIT_ENTITY, null, ORG_UNIT_ENTITY, ORG_UNIT_ENTITY_ID);
+    organizationUnitRoleNode.setParent(organizationRoleNode); //connect to parent node !!!
+    //store both nodes
+    List<RoleNode> resultOrg = given()
+        .log().all()
+        .contentType(ContentType.JSON)
+        .accept(MediaType.APPLICATION_JSON_DROPCHOP_RESULT)
+        .auth().preemptive().basic("admin1", "password")
+        .and()
+        .body(List.of(organizationRoleNode))
+        .when()
+        .post("/api" + INTERNAL_SEGMENT + ROLE_NODE)
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().jsonPath().getList("data", RoleNode.class);
+    assertEquals(1, resultOrg.size());
+    resultOrg = given()
+        .log().all()
+        .contentType(ContentType.JSON)
+        .accept(MediaType.APPLICATION_JSON_DROPCHOP_RESULT)
+        .auth().preemptive().basic("admin1", "password")
+        .and()
+        .body(List.of(organizationUnitRoleNode))
+        .when()
+        .post("/api" + INTERNAL_SEGMENT + ROLE_NODE + "?c_level=4")
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().jsonPath().getList("data", RoleNode.class);
+    assertEquals(1, resultOrg.size());
+    assertEquals(ORG_ROLE_NODE_ID, resultOrg.get(0).getParent().getUuid().toString());
+
+
+    //add template permissions for org unit to organization
+    List<Permission> permissions = this.getPermissions();
+    List<RoleNodePermission> roleNodeOrgUnitPermissions =
+        this.prepRoleNodePermissions(organizationRoleNode, permissions, ORG_UNIT_ENTITY, null, true);
+    //take one permission and change allowed state to override org unit defaults
+    RoleNodePermission deletePermission = roleNodeOrgUnitPermissions.stream()
+        .filter(p -> p.getPermission().getAction().equals(Constants.Actions.DELETE))
+        .map((RoleNodePermission p) -> {p.setAllowed(false); return p;}).findFirst().orElse(null);
+
+    List<RoleNodePermission> resultRoleNodeOrgUnitPermissions = given()
+        .log().all()
+        .contentType(ContentType.JSON)
+        .accept(MediaType.APPLICATION_JSON_DROPCHOP_RESULT)
+        .auth().preemptive().basic("admin1", "password")
+        .and()
+        .body(List.of(deletePermission))
+        .when()
+        .post("/api" + INTERNAL_SEGMENT + ROLE_NODE_PERMISSION)
+        .then()
+        .statusCode(200)
+        .extract()
+        .body().jsonPath().getList("data", RoleNodePermission.class);
+    assertEquals(1, resultRoleNodeOrgUnitPermissions.size());
+    for (RoleNodePermission p : resultRoleNodeOrgUnitPermissions) {
+      assertInstanceOf(RoleNodePermissionTemplate.class, p);
+      assertNotNull(((RoleNodePermissionTemplate)p).getTarget());
+    }
+
+  }
 
 
 }
