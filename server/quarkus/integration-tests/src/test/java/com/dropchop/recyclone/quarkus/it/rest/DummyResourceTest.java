4304b6d264d735af1a00a61e57be06089864c03f
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-03-27 14:59:06 +0100

    Fixing slow test and adding support for wait for elastic.
    https://github.com/dropchop-com/recyclone/issues/33

--- server/quarkus/integration-tests/src/test/java/com/dropchop/recyclone/quarkus/it/rest/DummyResourceTest.java
+++ server/quarkus/integration-tests/src/test/java/com/dropchop/recyclone/quarkus/it/rest/DummyResourceTest.java
@@ -8,10 +8,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -33,6 +30,13 @@ public class DummyResourceTest {
 
   @Test
   @Order(5)
+  @Tag("create")
+  @Tag("dummyRestGet")
+  @Tag("dummySearch")
+  @Tag("dummyQueryTestAggregations")
+  @Tag("dummyQueryTest")
+  @Tag("deleteById")
+  @Tag("deleteByQuery")
   public void create() {
     Dummy dummy1 = new Dummy();
     dummy1.setTitle("Introduction to Java");
@@ -80,6 +84,7 @@ public class DummyResourceTest {
 
   @Test
   @Order(10)
+  @Tag("dummyRestGet")
   public void dummyRestGet() {
     given()
       .log().all()
@@ -98,6 +103,7 @@ public class DummyResourceTest {
 
   @Test
   @Order(20)
+  @Tag("dummySearch")
   public void dummySearch() {
     CodeParams params = CodeParams.builder().code("dummy_code1").build();
     given()
@@ -114,83 +120,9 @@ public class DummyResourceTest {
     //.body("[0].code", equalTo("sl")).extract().asPrettyString();
   }
 
-  /*
   @Test
   @Order(30)
-  public void dummyQuery() {
-    QueryParams params = QueryParams.builder().condition(
-      and(
-        or(
-          field(
-            "updated",
-            gteLt(
-              Iso8601.fromIso("2024-09-19T10:12:01.123"),
-              Iso8601.fromIso("2024-09-20T11:00:01.123")
-            )
-          ),
-          and(
-            field("neki", in("one", "two", "three"))
-          ),
-          field("modified", Iso8601.fromIso("2024-09-19T10:12:01.123")),
-          not(
-            field(
-              "uuid", in("6ad7cbc2-fdc3-4eb3-bb64-ba6a510004db", "c456c510-3939-4e2a-98d1-3d02c5d2c609")
-            )
-          )
-        ),
-        field("type", in(1, 2, 3)),
-        field("created", Iso8601.fromIso("2024-09-19T10:12:01.123")),
-        field("miki", null)
-      ).and(
-        field("type2", in(1, 2, 3))
-      )
-    ).build();
-    given()
-      .log().all()
-      .contentType(ContentType.JSON)
-      .accept(MediaType.APPLICATION_JSON)
-      .auth().preemptive().basic("user1", "password")
-      .body(params)
-      .when()
-      .post("/api/public/test/dummy/query")
-      .then()
-      .statusCode(200)
-      .log().all();
-    //.body("[0].code", equalTo("sl")).extract().asPrettyString();
-  }
-
-  @Test
-  @Order(30)
-  public void dummyQueryTestAggregation() {
-    QueryParams params = QueryParams.builder().aggregation(aggs(
-      dateHistogram(
-        "watch_max",
-        "watch",
-        "seconds",
-        dateHistogram(
-          "nested_watch_max",
-          "watch",
-          "month"
-        )
-      )
-    )).build();
-    Log.debug(params.toString());
-    given()
-      .log().all()
-      .contentType(ContentType.JSON)
-      .accept(MediaType.APPLICATION_JSON)
-      .auth().preemptive().basic("user1", "password")
-      .body(params)
-      .when()
-      .post("/api/public/test/dummy/query")
-      .then()
-      .statusCode(200)
-      .log().all();
-    //.body("[0].code", equalTo("sl")).extract().asPrettyString();
-  }*/
-
-  @Test
-  @Order(30)
+  @Tag("dummyQueryTestAggregations")
   public void dummyQueryTestAggregations() {
     /*QueryParams params = QueryParams.builder().aggregation(
       aggs(
@@ -229,46 +161,6 @@ public class DummyResourceTest {
         )
       )
     ).build();*/
-    Dummy dummy1 = new Dummy();
-    dummy1.setTitle("Introduction to Java");
-    dummy1.setDescription("A comprehensive guide to Java programming.");
-    dummy1.setLang("en");
-    dummy1.setCreated(ZonedDateTime.now().minusDays(10));
-    dummy1.setModified(ZonedDateTime.now());
-    dummy1.setDeactivated(null);
-    dummy1.setCode("dummy_code23");
-
-    Dummy dummy2 = new Dummy();
-    dummy2.setTitle("Advanced Python Techniques");
-    dummy2.setDescription("Explore advanced concepts in Python programming.");
-    dummy2.setLang("en");
-    dummy2.setCreated(ZonedDateTime.now().minusDays(20));
-    dummy2.setModified(ZonedDateTime.now().minusDays(5));
-    dummy2.setDeactivated(null);
-    dummy2.setCode("dummy_code24");
-
-    Dummy dummy3 = new Dummy();
-    dummy3.setTitle("Introduction to Machine Learning");
-    dummy3.setDescription("An introductory course to machine learning and its applications.");
-    dummy3.setLang("si");
-    dummy3.setCreated(ZonedDateTime.now().minusMonths(2));
-    dummy3.setModified(ZonedDateTime.now().minusDays(10));
-    dummy3.setDeactivated(null);
-    dummy3.setCode("dummy_code35");
-
-    List<Dummy> dummies = List.of(dummy1, dummy2, dummy3);
-
-    given()
-      .log().all()
-      .contentType(ContentType.JSON)
-      .accept(MediaType.APPLICATION_JSON)
-      .auth().preemptive().basic("editor1", "password")
-      .body(dummies)
-      .when()
-      .post("/api/internal/test/dummy")
-      .then()
-      .statusCode(200)
-      .log().all();
 
     QueryParams params = QueryParams.builder().aggregation(
       aggs(
@@ -379,6 +271,7 @@ public class DummyResourceTest {
 
   @Test
   @Order(50)
+  @Tag("dummyQueryTest")
   public void dummyQueryTest() {
     QueryParams params = QueryParams.builder().condition(
       and(
@@ -423,6 +316,7 @@ public class DummyResourceTest {
 
   @Test
   @Order(30)
+  @Tag("dummySearchNullTest")
   public void dummySearchNullTest() {
     QueryParams params = QueryParams.builder().condition(
       field("languages.name", "si")
@@ -446,7 +340,7 @@ public class DummyResourceTest {
   public void dummySearchOneDummyTest() {
     QueryParams params = QueryParams.builder().condition(
       and(
-        field("code.keyword", "JAVA101"),
+        field("code", "JAVA101"),
         field("lang", "en")
       )
     ).build();
@@ -664,6 +558,7 @@ public class DummyResourceTest {
 
   @Test
   @Order(60)
+  @Tag("deleteById")
   public void deleteById() {
     CodeParams params1 = CodeParams.builder().code("dummy_code1").build();
 
@@ -682,9 +577,10 @@ public class DummyResourceTest {
 
   @Test
   @Order(70)
+  @Tag("deleteByQuery")
   public void deleteByQuery() {
     QueryParams s = QueryParams.builder().condition(
-      field("lang", "si")
+      field("code", "dummy_code2")
     ).build();
 
     given()
