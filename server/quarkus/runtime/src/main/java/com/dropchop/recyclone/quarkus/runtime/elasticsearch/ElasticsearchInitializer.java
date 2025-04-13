659c9297b8bc8446c7c0239becb7f3402d34ca44
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-04-13 20:27:06 +0200

    Implemented profile based data Elasticsearch initialization. Added Elasticsearch templates out-of-container and used vanilla Elasticsearch. Added all the templates. Removed: - DockerIntegrationITTest.java - JPA configs - Invalid IndexConfig.java
    https://github.com/dropchop-com/klipingmm/issues/2720

--- server/quarkus/runtime/src/main/java/com/dropchop/recyclone/quarkus/runtime/elasticsearch/ElasticsearchInitializer.java
+++ server/quarkus/runtime/src/main/java/com/dropchop/recyclone/quarkus/runtime/elasticsearch/ElasticsearchInitializer.java
@@ -5,12 +5,12 @@ import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -99,7 +99,7 @@ public class ElasticsearchInitializer {
       new Data()
   );
 
-  private void loadTemplateResources() throws IOException {
+  private void loadTemplateResources(String profileKey) throws IOException {
     ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     for (Templates templates : templatesList) {
       Enumeration<URL> resources = classLoader.getResources(templates.resourcePath);
@@ -109,6 +109,10 @@ public class ElasticsearchInitializer {
           String resource;
           log.debug("Reading template folder [{}]", resourceUrl);
           while ((resource = br.readLine()) != null) {
+            if (resource.startsWith("%") && !resource.startsWith("%" + profileKey + ".")) {
+              log.debug("Skipping profile [{}] template [{}]", profileKey, resource);
+              continue;
+            }
             String templatePath = templates.resourcePath + "/" + resource;
             log.debug("Reading template [{}]", resource);
             try (InputStream is = classLoader.getResourceAsStream(templatePath)) {
@@ -146,25 +150,104 @@ public class ElasticsearchInitializer {
     }
   }
 
-  private void applyTemplate(String templateUrl, String templateSource) throws IOException {
-    Request request = new Request("PUT", templateUrl);
+  private void apply(String templateUrl, String templateSource, String method) throws IOException {
+    Request request = new Request(method, templateUrl);
     request.setJsonEntity(templateSource);
     restClient.performRequest(request);
   }
 
+  private void applyTemplate(String templateUrl, String templateSource) throws IOException {
+    apply(templateUrl, templateSource, "PUT");
+  }
+
+  private String extractSource(String doc) {
+    String sourceField = "\"_source\":";
+    int idx = doc.indexOf(sourceField);
+    if (idx == -1) {
+      return null;
+    }
+    idx = doc.indexOf('{', idx);
+    if (idx == -1) {
+      return null;
+    }
+    return doc.substring(idx, doc.length() - 1);
+  }
+
+  private String extractFieldValue(String fieldName, String doc) {
+    int idx = doc.indexOf(fieldName);
+    if (idx == -1) {
+      return null;
+    }
+    idx = doc.indexOf('"', idx + fieldName.length() + 1); // third " in "_field_name": "value"
+    if (idx == -1) {
+      return null;
+    }
+    int endIdx = doc.indexOf('"', idx + 1); // fourth " in "_field_name": "value"
+    return doc.substring(idx + 1, endIdx);
+  }
+
+  private void applyData(Template template, String baseUrl) throws IOException {
+    List<String> lines = Arrays.asList(template.template.split("\n"));
+    int chunkSize = 2500;
+    int chunkIdx = 0;
+    int maxChunkIdx = lines.size() / chunkSize;
+    for (int i = 0; i < lines.size(); i += chunkSize) {
+      List<String> chunk = lines.subList(i, Math.min(i + chunkSize, lines.size()));
+      String bulkRequestBody;
+      if (template.templatePath.endsWith(".json")) {
+        // why would it be simple? this is an elastic dump file with index doc per line
+        StringBuilder bulkBody = new StringBuilder();
+        for (String line : chunk) {
+          String indexName = extractFieldValue("_index", line);
+          String id = extractFieldValue("_id", line);
+          String sourceString = extractSource(line);
+
+          bulkBody.append("{ \"index\" : { \"_index\" : \"");
+          bulkBody.append(indexName);
+          bulkBody.append("\", \"_id\" : \"");
+          bulkBody.append(id);
+          bulkBody.append("\" } }\n");
+          bulkBody.append(sourceString);
+          bulkBody.append("\n");
+        }
+        bulkRequestBody = bulkBody.toString();
+      } else {
+        // assuming the proper bulk format
+        bulkRequestBody = String.join("\n", chunk);
+      }
+      if (chunkIdx == maxChunkIdx) { // last chunk
+        apply(baseUrl + "?refresh=wait_for", bulkRequestBody, "POST");
+      } else {
+        apply(baseUrl, bulkRequestBody, "POST");
+      }
+      log.debug("Applied data [{}][{}]", template.name, chunkIdx);
+      chunkIdx++;
+    }
+  }
+
   private void applyMissingTemplates() {
     for (Templates templates : templatesList) {
-      for(Template template : templates.templates) {
-        String templateUrl = templates.baseUrl + "/" + template.name;
-        try {
-          log.debug("Applying template [{}]", templateUrl);
-          if (!checkTemplateExists(templateUrl)) {
-            applyTemplate(templateUrl, template.template);
-          } else {
-            log.info("Template [{}] already exists", templateUrl);
+      if (templates instanceof Data dataTemplate) {
+        for (Template template : templates.templates) {
+          try {
+            applyData(template, dataTemplate.baseUrl);
+          } catch (IOException e) {
+            log.error("Failed to apply data template [{}]!", template.name, e);
           }
-        } catch (IOException e) {
-          log.error("Failed to apply template [{}]!", templateUrl, e);
+        }
+      } else {
+        for (Template template : templates.templates) {
+          String templateUrl = templates.baseUrl + "/" + template.name;
+          try {
+            log.debug("Applying template [{}]", templateUrl);
+            if (!checkTemplateExists(templateUrl)) {
+              applyTemplate(templateUrl, template.template);
+            } else {
+              log.info("Template [{}] already exists", templateUrl);
+            }
+          } catch (IOException e) {
+            log.error("Failed to apply template [{}]!", templateUrl, e);
+          }
         }
       }
     }
@@ -174,7 +257,8 @@ public class ElasticsearchInitializer {
     if (!LaunchMode.current().isDevOrTest()) {
       return;
     }
-    loadTemplateResources();
+    String profileKey = LaunchMode.current().getDefaultProfile();
+    loadTemplateResources(profileKey);
     applyMissingTemplates();
   }
 }
