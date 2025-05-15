73a6c8c591343e53058981519b6123bf776199bc
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-05-15 07:43:14 +0200

    Add delete method to UserService with default restriction
    Introduced an override for the delete method in UserService that throws a ServiceException, disallowing user deletions by default. This ensures stricter control over user removal in the base implementation.

--- server/base/jpa/service/src/main/java/com/dropchop/recyclone/base/jpa/service/security/UserService.java
+++ server/base/jpa/service/src/main/java/com/dropchop/recyclone/base/jpa/service/security/UserService.java
@@ -1,8 +1,11 @@
 package com.dropchop.recyclone.base.jpa.service.security;
 
-import com.dropchop.recyclone.base.api.model.invoke.CommonExecContext;
-import com.dropchop.recyclone.base.api.service.CrudServiceImpl;
 import com.dropchop.recyclone.base.api.common.RecycloneType;
+import com.dropchop.recyclone.base.api.model.invoke.CommonExecContext;
+import com.dropchop.recyclone.base.api.model.invoke.ErrorCode;
+import com.dropchop.recyclone.base.api.model.invoke.ServiceException;
+import com.dropchop.recyclone.base.api.service.CrudServiceImpl;
+import com.dropchop.recyclone.base.dto.model.rest.Result;
 import com.dropchop.recyclone.base.dto.model.security.User;
 import com.dropchop.recyclone.base.jpa.model.security.JpaUser;
 import com.dropchop.recyclone.base.jpa.repo.security.UserMapperProvider;
@@ -11,6 +14,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 import static com.dropchop.recyclone.base.api.model.marker.Constants.Implementation.RECYCLONE_DEFAULT;
@@ -30,4 +34,9 @@ public class UserService extends CrudServiceImpl<User, JpaUser, UUID>
 
   @Inject
   CommonExecContext<User, ?> executionContext;
+
+  @Override
+  public Result<User> delete(List<User> dtos) {
+    throw new ServiceException(ErrorCode.process_error, "Not allowed to delete users by default implementation!");
+  }
 }
