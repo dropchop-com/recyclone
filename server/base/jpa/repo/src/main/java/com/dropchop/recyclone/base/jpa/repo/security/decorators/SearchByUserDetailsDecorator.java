8de35f59f90c8b9f5d269e0305855f3bb66cedac
Armando Ota <armando.ota@dropchop.com>
2025-07-12 12:57:24 +0200

    Added loading by username condition.

--- server/base/jpa/repo/src/main/java/com/dropchop/recyclone/base/jpa/repo/security/decorators/SearchByUserDetailsDecorator.java
+++ server/base/jpa/repo/src/main/java/com/dropchop/recyclone/base/jpa/repo/security/decorators/SearchByUserDetailsDecorator.java
@@ -9,19 +9,25 @@ import com.dropchop.recyclone.base.jpa.model.security.JpaLoginAccount;
 import com.dropchop.recyclone.base.jpa.model.security.JpaTokenAccount;
 import com.dropchop.recyclone.base.jpa.repo.BlazeCriteriaDecorator;
 
-public class SearchByUserDetailsDecorator <E> extends BlazeCriteriaDecorator<E> {
+public class SearchByUserDetailsDecorator<E> extends BlazeCriteriaDecorator<E> {
 
   @Override
   public void decorate() {
     String alias = getContext().getRootAlias();
     Params params = getContext().getParams();
     if (params instanceof UserParams userParams) {
+      String loginName = userParams.getLoginName();
       String firstName = userParams.getFirstName();
       String lastName = userParams.getLastName();
       String email = userParams.getEmail();
       String searchTerm = userParams.getSearchTerm();
 
       CriteriaBuilder<?> cb = getContext().getCriteriaBuilder();
+      if (loginName != null && !loginName.isBlank()) {
+        cb.join("accounts", "a", JoinType.LEFT);
+        cb.where("TREAT(a AS " + JpaLoginAccount.class.getSimpleName() + ")"
+          + DELIM + SearchFields.User.LOGIN_NAME).eq(loginName);
+      }
       if (firstName != null && !firstName.isBlank()) {
         cb.where(alias + DELIM + "firstName").eq(firstName);
       }
@@ -39,9 +45,9 @@ public class SearchByUserDetailsDecorator <E> extends BlazeCriteriaDecorator<E> 
           .where("LOWER(" + alias + DELIM + "lastName)").like().value(searchTerm.toLowerCase() + '%').noEscape()
           .where("LOWER(" + alias + DELIM + "defaultEmail)").like().value(searchTerm.toLowerCase() + '%').noEscape()
           .where("TREAT(a AS " + JpaLoginAccount.class.getSimpleName() + ")"
-            + DELIM + SearchFields.User.LOGIN_NAME).eq(searchTerm.toLowerCase())
+            + DELIM + SearchFields.User.LOGIN_NAME).eq(searchTerm)
           .where("TREAT(t AS " + JpaTokenAccount.class.getSimpleName() + ")"
-            + DELIM + SearchFields.User.TOKEN).eq(searchTerm.toLowerCase()).endOr();
+            + DELIM + SearchFields.User.TOKEN).eq(searchTerm).endOr();
       }
     }
   }
