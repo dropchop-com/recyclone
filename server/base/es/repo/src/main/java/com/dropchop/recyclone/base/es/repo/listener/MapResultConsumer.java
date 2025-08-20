ee5a5e2c9ccb701b621b311f0bba2526d0d56ace
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-08-20 08:56:22 +0200

    Unified repository result listener to common ResultConsumer
    https://github.com/dropchop-com/klipingmm/issues/2721

--- server/base/es/repo/src/main/java/com/dropchop/recyclone/base/es/repo/listener/MapResultConsumer.java
+++ server/base/es/repo/src/main/java/com/dropchop/recyclone/base/es/repo/listener/MapResultConsumer.java
@@ -5,6 +5,5 @@ import java.util.Map;
 /**
  * @author Samo Pritrznik <samo.pritrznik@dropchop.com> on 5. 12. 24
  **/
-public interface MapResultListener extends QueryResultListener<Map<String, ?>> {
-  Progress onResult(Map<String, ?> result);
+public interface MapResultConsumer extends QueryResultConsumer<Map<String, ?>> {
 }
