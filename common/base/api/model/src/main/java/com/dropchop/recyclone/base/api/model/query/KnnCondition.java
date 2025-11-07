0eeb0a58be412e8f39a0605b285e4796dea41cfd
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-11-07 16:38:14 +0100

    Refactored KNN types and handling.
    https://github.com/dropchop-com/clippitamine/issues/50
    https://github.com/dropchop-com/clippitamine/issues/86

--- common/base/api/model/src/main/java/com/dropchop/recyclone/base/api/model/query/KnnCondition.java
+++ common/base/api/model/src/main/java/com/dropchop/recyclone/base/api/model/query/KnnCondition.java
@@ -4,19 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * @author Nikola Ivačič <nikola.ivacic@dropchop.com> on 26. 08. 2025
+ * @author Nikola Ivačič <nikola.ivacic@dropchop.com> on 07. 11. 2025.
  */
 @Getter
 @Setter
-public class KnnField implements Condition {
+public class KnnCondition<T> implements Condition {
   private String name;
-  private float[] value;
+  private T value;
   private Integer topK;
   private Float similarity;
   private Integer numCandidates;
   private Condition filter;
 
-  public KnnField(String name, float[] val, Integer topK, Float similarity, Integer numCandidates, Condition filter) {
+  public KnnCondition(String name, T val, Integer topK, Float similarity, Integer numCandidates, Condition filter) {
     this.name = name;
     this.value = val;
     this.topK = topK;
@@ -25,31 +25,31 @@ public class KnnField implements Condition {
     this.filter = filter;
   }
 
-  public KnnField(String name, float[] val, Integer topK, Float similarity,  Integer numCandidates) {
+  public KnnCondition(String name, T val, Integer topK, Float similarity, Integer numCandidates) {
     this(name, val, topK, similarity, numCandidates, null);
   }
 
-  public KnnField(String name, float[] val, Integer topK, Float similarity, Condition filter) {
+  public KnnCondition(String name, T val, Integer topK, Float similarity, Condition filter) {
     this(name, val, topK, similarity, null, filter);
   }
 
-  public KnnField(String name, float[] val, Integer topK, Condition filter) {
+  public KnnCondition(String name, T val, Integer topK, Condition filter) {
     this(name, val, topK, null, null, filter);
   }
 
-  public KnnField(String name, float[] val, Condition filter) {
+  public KnnCondition(String name, T val, Condition filter) {
     this(name, val, null, null, null, filter);
   }
 
-  public KnnField(String name, float[] val, Integer topK, Float similarity) {
+  public KnnCondition(String name, T val, Integer topK, Float similarity) {
     this(name, val, topK, similarity, null, null);
   }
 
-  public KnnField(String name, float[] val, Integer topK) {
+  public KnnCondition(String name, T val, Integer topK) {
     this(name, val, topK, null, null, null);
   }
 
-  public KnnField(String name, float[] val) {
+  public KnnCondition(String name, T val) {
     this(name, val, null, null, null, null);
   }
 }
