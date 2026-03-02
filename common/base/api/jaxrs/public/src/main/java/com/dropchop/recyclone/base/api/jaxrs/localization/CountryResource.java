46d26e75367e8f29c5d93cffb16f88604d76468d
Nikola Ivačič <nikola.ivacic@dropchop.com>
2026-03-02 13:48:10 +0100

    Added support for title search for countries and languages to reuse rest api downstream.

--- common/base/api/jaxrs/public/src/main/java/com/dropchop/recyclone/base/api/jaxrs/localization/CountryResource.java
+++ common/base/api/jaxrs/public/src/main/java/com/dropchop/recyclone/base/api/jaxrs/localization/CountryResource.java
@@ -1,15 +1,14 @@
 package com.dropchop.recyclone.base.api.jaxrs.localization;
 
 import com.dropchop.recyclone.base.api.model.rest.Constants.Paths;
+import com.dropchop.recyclone.base.api.model.rest.DynamicExecContext;
+import com.dropchop.recyclone.base.api.model.rest.MediaType;
 import com.dropchop.recyclone.base.api.model.security.Constants.Actions;
 import com.dropchop.recyclone.base.api.model.security.Constants.Domains;
 import com.dropchop.recyclone.base.api.model.security.annotations.RequiresPermissions;
-import com.dropchop.recyclone.base.dto.model.invoke.CodeParams;
 import com.dropchop.recyclone.base.dto.model.invoke.CodeTitleParams;
 import com.dropchop.recyclone.base.dto.model.localization.Country;
 import com.dropchop.recyclone.base.dto.model.rest.Result;
-import com.dropchop.recyclone.base.api.model.rest.DynamicExecContext;
-import com.dropchop.recyclone.base.api.model.rest.MediaType;
 import jakarta.ws.rs.*;
 
 import java.util.List;
@@ -20,7 +19,7 @@ import static com.dropchop.recyclone.base.api.model.security.Constants.PERM_DELI
  * @author Nikola Ivačič <nikola.ivacic@dropchop.com> on 12. 01. 22.
  */
 @Path(Paths.Localization.COUNTRY)
-@DynamicExecContext(CodeParams.class)
+@DynamicExecContext(CodeTitleParams.class)
 @RequiresPermissions(Domains.Localization.COUNTRY + PERM_DELIM + Actions.VIEW)
 public interface CountryResource {
 
